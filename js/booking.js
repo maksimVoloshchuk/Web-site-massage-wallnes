5c86920c4acd9f97e9610038c50e3390ae00a66b
Maksim Voloshchuk <maksimvolosuk684@gmail.com>
2026-04-14 02:27:09 +0200

    add correct supabase key

--- js/booking.js
+++ js/booking.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://uppduzvyirfyhvapjdz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwZGR1enZ5aXJmdnlodmFwamR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxMTA3MzIsImV4cCI6MjA5MTY4NjczMn0.6kUkf7e_I08FgFq7Ptqb0ksfJYyocJPDKJX0UltDiCs'
+const supabaseKey = 'sb_publishable_RIzN7JdnVA4Bn6-tfYKSug_gC8HsKeZ'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
