8488c05a06cb1cda76d7d2881e2e7264c46ed80c
Maksim Voloshchuk <maksimvolosuk684@gmail.com>
2026-04-14 02:31:51 +0200

    fix correct supabase url finally

--- js/booking.js
+++ js/booking.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://uppduzvyirfyhvapjdz.supabase.co'
+const supabaseUrl = 'https://upddzuvyirfyhvapjdz.supabase.co'
 const supabaseKey = 'sb_publishable_RIzN7JdnVA4Bn6-tfYKSug_gC8HsKeZ'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
